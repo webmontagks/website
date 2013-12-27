e8b7fc3ddc4e73d7e070bcbe1e45f37e2d5352bd
Michael Kuehnel <mail@michael-kuehnel.de>
2013-12-27 22:10:17 +0100

    Add grunt watch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
 module.exports = function(grunt) {
 
-	// 1. All configuration goes here
+	// Config
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		// concat
 		concat: {
 			// 2. Configuration for concatinating files goes here.
 			dist: {
@@ -12,6 +13,7 @@ module.exports = function(grunt) {
 			}
 		},
 
+		// uglify
 		uglify: {
 			options: {
 				// preserveComments: 'some'
@@ -23,6 +25,7 @@ module.exports = function(grunt) {
 			}
 		},
 
+		// imagemin
 		imagemin: {
 			dist: {
 				files: [{
@@ -34,6 +37,7 @@ module.exports = function(grunt) {
 			}
 		},
 
+		// less
 		less: {
 			development: {
 				options: {
@@ -52,17 +56,52 @@ module.exports = function(grunt) {
 					"dist/css/main.min.css": "src/less/main.less"
 				}
 			}
+		},
+
+		// watch
+		watch: {
+			options: {
+				livereload: true,
+			},
+			scripts: {
+				files: ['src/js/*.js'],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+			css: {
+				files: ['src/less/*.less'],
+				tasks: ['less'],
+				options: {
+					spawn: false,
+				}
+			},
+			html: {
+				files: ['*.html'],
+				options: {
+					spawn: false,
+				}
+			},
+			images: {
+				files: ['src/img/**/*.{png,jpg,gif}'],
+				tasks: ['imagemin'],
+				options: {
+					spawn: false,
+				}
+			}
 		}
 
 	});
 
-	// 3. Where we tell Grunt we plan to use this plug-in.
+	// Load plugins.
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	// Define tasks.
 	grunt.registerTask('default', ['less', 'concat', 'uglify', 'imagemin']);
 
 };
